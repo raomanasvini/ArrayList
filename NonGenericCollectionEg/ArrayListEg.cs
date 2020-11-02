de6f5b8b6a56eaf8711f0c9e7a10915f5131eb9b
raomanasvini <Manasvini98.rao@gmail.com>
2020-11-02 14:20:29 +0530

    ArrayList

---
+++ NonGenericCollectionEg/ArrayListEg.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace NonGenericCollectionEg
+{
+    class ArrayListEg
+    {
+        static void Main()
+        {
+            int[] arr = { 90, 60, 89, 10, 34 };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+
+            }
+            Console.WriteLine("After Sorting");
+            Array.Sort(arr);
+            //for(int i= 0;i<arr.Length;i++)
+            //foreach(element in iterable -item)
+            //var :implicit typed variable
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(".........ArrayList.........");
+            ArrayList al = new ArrayList();
+            al.Add(10);
+            al.Add(80.98);
+            al.Add("Kalai");
+            al.Add(90);
+            al.Add("Kalai");
+            foreach (var item in al)
+            {
+                Console.WriteLine("ArrayList: {0}", item);
+            }
+            Console.WriteLine("Count: {0}", al.Count);
+            Console.WriteLine("Capacity: {0}", al.Capacity);
+            Console.WriteLine("Contains: {0}", al.Contains("Lalitha"));
+            al.Remove(80.98);
+            foreach (var item in al)
+            {
+                Console.WriteLine("ArrayList: {0}", item);
+
+            }
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ NonGenericCollectionEg/HashtableEg.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace NonGenericCollectionEg
+{
+    class HashtableEg
+    {
+        static void Main()
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add(1, "Monday");
+            ht.Add(3, "Wednesday");
+            ht.Add(4, "thursday");
+            ht.Add("f", "Friday");
+            ht.Add("fri", "Friday");
+
+            foreach (DictionaryEntry d in ht)
+            {
+                Console.WriteLine(d. Key + " " + d. Value);
+            }
+
+            Console.WriteLine(".........SortedList.........");
+            SortedList st = new SortedList();
+            st.Add(1, "Monday");
+            st.Add(4, "thursday");
+            st.Add(3, "Wednesday");
+            SortedList st1 = new SortedList();
+            st1. Add("m", "Manasvini");
+            st1. Add("r",  "Renu");
+
+            foreach (DictionaryEntry di in st1)
+            {
+                Console.WriteLine(di. Key + " " + di. Value);
+
+            }
+            Console.Read();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+}
